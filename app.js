e34ee2281901dad5f8bc63c11c91c9c93dea5439
Ajith Chettiyar <112484343+Riderajit@users.noreply.github.com>
2024-11-08 12:38:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('CI/CD with ECR in EC2 instance!');
+  res.send('CI/CD with github action!');
 });
 
 app.listen(port, () => {
